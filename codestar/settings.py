9e218b99507313c5eff278ab56f6fd7ee06f71b3
Pegrume88 <pegrume88@gmail.com>
2022-11-14 10:52:11 +0000

    fixed allowed host error

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar-blog-rhys.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['codestar-blog-rhys.herokuapp.com', 'localhost']
 
 
 # Application definition
