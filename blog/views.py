6d6d24d8f01fe543bb82395a13bd545f35e659a1
Pegrume88 <pegrume88@gmail.com>
2022-11-22 20:25:17 +0000

    add messages

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -62,4 +64,17 @@ class PostDetail(View):
                 "comment_form": comment_form,
                 "liked": liked
             },
+            messages.success(request, 'Your profile was updated.')
         )
+
+
+class PostLike(View):
+    
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
